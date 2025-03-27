83f745a19214668ac7736ce6167627411ccf4c61
Uzanyme <guillaume.card2brain@gmail.com>
2025-03-27 16:52:20 +0100

    FireHand : the fireBall have a finite life time

--- Assets/Script/FireBall.cs
+++ Assets/Script/FireBall.cs
@@ -6,11 +6,14 @@ public class FireBall : MonoBehaviour
     private bool isFalling = false;
     private float fallingDelayTime = 3.0f;
     private bool hasFallen = false;
+
+    [SerializeField] private float maxLifeTime = 30.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GetComponent<Rigidbody>().useGravity = false;
         StartCoroutine(fallingDelay());
+        StartCoroutine(lifeTime());
         
     }
 
@@ -33,5 +36,11 @@ public class FireBall : MonoBehaviour
     {
         yield return new WaitForSeconds(fallingDelayTime);
         isFalling = true;
+    }
+
+    private IEnumerator lifeTime()
+    {
+        yield return new WaitForSeconds(maxLifeTime);
+        Destroy(gameObject);
     }
 }
